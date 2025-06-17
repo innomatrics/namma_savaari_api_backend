74bfd0ca1a9ada4b1b9382d35b9dd2803f7a1dc1
saneeswaran <srisrisrisathish176@gmail.com>
2025-06-17 03:10:10 -0700

    changed search

--- controller/search.js
+++ controller/search.js
@@ -2,20 +2,16 @@ const { search } = require("../global_variable");
 const axios = require("axios");
 
 exports.search = async (req, res) => {
-  const body = req.body;
-
   try {
-    const response = await axios.post(search, body, {
+    const response = await axios.post(search, req.body, {
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': req.headers['api-token'], 
+        'Authorization': req.headers['api-token'],
       },
     });
-
     res.status(response.status).json(response.data);
   } catch (error) {
     console.error("Search proxy error:", error?.response?.data || error.message);
-
     res.status(error.response?.status || 500).json({
       error: 'Proxy failed',
       backendMessage: error.response?.data || {},
