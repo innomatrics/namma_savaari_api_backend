cc1d9fde09ff326d5a3fcf5e6f7e6a34167a37b7
saneeswaran <srisrisrisathish176@gmail.com>
2025-06-17 05:16:29 -0700

    changed search

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-// server.js
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
 // Routers
 const cityListRouter = require("./routes/city_list");
@@ -14,22 +17,20 @@ const search = require("./routes/search");
 const block = require("./routes/block");
 const balance = require("./routes/balance");
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-
 app.use("/", cityListRouter);
 app.use("/", balanceLog);
-app.use("/", bookBus);
+app.use("/bus-api", bookBus);   
+app.use("/bus-api", block);     
 app.use("/", cancelBooking);
 app.use("/", getBoardingPoint);
 app.use("/", getSeatLayout);
 app.use("/", search);
-app.use("/", block);
 app.use("/", balance);
 
 app.get("/", (req, res) => {
   res.send("Hello from the proxy server");
 });
+
 app.listen(3000, () => console.log("Proxy server listening on port 3000"));
+
 module.exports = app;
